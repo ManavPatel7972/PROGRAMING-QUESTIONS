29e5e728d19d5173be1ab09158027ff5bb1c9428
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-06-24 19:49:31 +0530

    Add Find Peak Element Implementation

---
+++ BinarySearchArray/FindPeakEle.java
@@ -0,0 +1,46 @@
+public class FindPeakEle {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 5, 1 };
+        System.out.println("ELE = " + findPeakOptimal(arr));
+    }
+
+    public static int findPeakBrute(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            if ((i == 0 || arr[i] > arr[i - 1]) && (i == n - 1 || arr[i] > arr[i + 1])) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    public static int findPeakOptimal(int[] arr) {
+        int n = arr.length;
+        int low = 1;
+        int high = n - 2;
+
+        if (n == 1)
+            return arr[0];
+        if (arr[0] > arr[1])
+            return arr[0];
+        if (arr[n - 1] > arr[n - 2])
+            return arr[n - 1];
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                return arr[mid];
+
+            else if (arr[mid] > arr[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
