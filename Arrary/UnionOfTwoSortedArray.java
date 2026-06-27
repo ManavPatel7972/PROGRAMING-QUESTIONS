85f26aea86d6a4d8e61255679ed44189048db2da
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-06-27 09:16:43 +0530

    Refactor Union of Two Sorted Arrays Implementation for Clarity and Consistency

--- Arrary/UnionOfTwoSortedArray.java
+++ Arrary/UnionOfTwoSortedArray.java
@@ -11,9 +11,9 @@
 
 //         int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 //         int[] arr2 = {2, 3, 4, 4, 5, 11, 12};
-    
+
 //         List<Integer> Union = findUnion(arr1, arr2, n, m);
-    
+
 //         System.out.println("Union of arr1 and arr2 is ");
 //         for (int val : Union)
 //             System.out.print(val + " ");
@@ -23,7 +23,7 @@
 //     public static List<Integer> findUnion(int[] arr1, int[] arr2, int n, int m){
 
 //         TreeMap<Integer, Integer> fre = new TreeMap<>();
-        
+
 //         for(int i=0;i<n;i++){
 //             fre.put(arr1[i], fre.getOrDefault(arr1[i], 0)+1);
 //         }
@@ -41,7 +41,6 @@
 //     }   
 // }
 
-
 // --------------------------OPTIMAL-------------
 
 import java.util.ArrayList;
@@ -49,70 +48,68 @@ import java.util.List;
 
 public class UnionOfTwoSortedArray {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] arr2 = {2, 3, 4, 4, 5, 11, 12};
+        int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] arr2 = { 2, 3, 4, 4, 5, 11, 12 };
         int n = arr1.length, m = arr2.length;
 
         List<Integer> result = findUnion(arr1, arr2, n, m);
 
         System.out.print("Union of arr1 and arr2 is: ");
-        for (int val : result) System.out.print(val + " ");
+        for (int val : result)
+            System.out.print(val + " ");
 
     }
 
-    public static List<Integer> findUnion(int[] arr1, int[] arr2, int n, int m){
+    public static List<Integer> findUnion(int[] arr1, int[] arr2, int n, int m) {
 
-       List<Integer> uni = new ArrayList<>();
+        List<Integer> uni = new ArrayList<>();
 
-       int i=0, j=0;
+        int i = 0, j = 0;
 
-       while (i<n && j<m) {
-            if(arr1[i] < arr2[j]){
-                if (uni.isEmpty() || uni.get(uni.size()-1)!= arr1[i]) {
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                if (uni.isEmpty() || uni.get(uni.size() - 1) != arr1[i]) {
                     uni.add(arr1[i]);
                 }
                 i++;
-            }
-            else if(arr2[j] < arr1[i]){
-                if(uni.isEmpty() || uni.get(uni.size()-1)!= arr2[j]){
+            } else if (arr2[j] < arr1[i]) {
+                if (uni.isEmpty() || uni.get(uni.size() - 1) != arr2[j]) {
                     uni.add(arr2[j]);
-                   
+
                 }
-                 j++;
-            }
-            else{
+                j++;
+            } else {
                 // Elements are equal add only ones (any i or j)
-                if(uni.isEmpty() || uni.get(uni.size()-1)!= arr1[i]){
+                if (uni.isEmpty() || uni.get(uni.size() - 1) != arr1[i]) {
                     uni.add(arr1[i]);
-                    //  Move both pointers
-                   
+                    // Move both pointers
+
                 }
-                 i++;
+                i++;
                 j++;
             }
-       }
+        }
 
-    // Append remaining ele from arr1
-        while (i< n) {
-            if(uni.isEmpty() || uni.get(uni.size()-1)!=arr1[i]){
-                 uni.add(arr1[i]);
-                 
+        // Append remaining ele from arr1
+        while (i < n) {
+            if (uni.isEmpty() || uni.get(uni.size() - 1) != arr1[i]) {
+                uni.add(arr1[i]);
+
             }
             i++;
 
         }
 
-         while (j < m) {
-            if(uni.isEmpty() || uni.get(uni.size()-1) !=arr2[j]){
-                 uni.add(arr2[j]);
-                
+        while (j < m) {
+            if (uni.isEmpty() || uni.get(uni.size() - 1) != arr2[j]) {
+                uni.add(arr2[j]);
+
             }
-             j++;
+            j++;
 
         }
 
         return uni;
 
-
-    }   
+    }
 }
