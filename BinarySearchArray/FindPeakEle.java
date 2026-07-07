58a17a0f14b16f89e613eeec58957aefc314ad80
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-07-07 19:49:39 +0530

    Refactor findPeakOptimal method for improved boundary checks and clarity

--- BinarySearchArray/FindPeakEle.java
+++ BinarySearchArray/FindPeakEle.java
@@ -18,23 +18,22 @@ public class FindPeakEle {
 
     public static int findPeakOptimal(int[] arr) {
         int n = arr.length;
-        int low = 1;
-        int high = n - 2;
 
         if (n == 1)
-            return arr[0];
-        if (arr[0] > arr[1])
-            return arr[0];
-        if (arr[n - 1] > arr[n - 2])
-            return arr[n - 1];
+            return 0;
+
+        int low = 0;
+        int high = n - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
-                return arr[mid];
+            if ((mid == 0 || arr[mid] > arr[mid - 1]) &&
+                    (mid == n - 1 || arr[mid] > arr[mid + 1])) {
+                return mid;
+            }
 
-            else if (arr[mid] > arr[mid - 1]) {
+            if (mid < n - 1 && arr[mid] < arr[mid + 1]) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
