a9f03dc06b7bb1941d9a69174b54e1b1d682126f
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-07-25 17:24:16 +0530

    Add PrintNameNTimes implementation to print a name multiple times using recursion

---
+++ Recursion/PrintNameNTimes.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class PrintNameNTimes {
+    public static void main(String[] args) {
+        int n = 10;
+        print(1, n);
+    }
+
+    public static void print(int i, int n) {
+        if (i > n)
+            return;
+
+        System.out.println("Hello Manuu.." + i);
+
+        print(i + 1, n);
+    }
+
+}
