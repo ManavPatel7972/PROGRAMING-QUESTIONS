64ac0e09e9f7ff3942a3dcb5744ab04fa307f069
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-08-18 19:00:32 +0530

    Refactor BFS implementation for improved readability and maintainability

--- Graph/Bfs.java
+++ Graph/Bfs.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -6,24 +7,22 @@ import java.util.Scanner;
 public class Bfs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int V = sc.nextInt();
         int E = sc.nextInt();
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for(int i=0;i<=V;i++){
+        for (int i = 0; i <= V; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for(int i=0;i<E;i++){
+        for (int i = 0; i < E; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
 
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-
 
         ArrayList<Integer> bfs = bfsOfGraph(V, adj);
 
@@ -34,14 +33,37 @@ public class Bfs {
         sc.close();
     }
 
-    public static ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){
-        
+    public static ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        // ArrayList<Integer> bfs = new ArrayList<>();
+        // boolean[] vis = new boolean[V];
+
+        // Queue<Integer> q = new LinkedList<>();
+        // // starting Node
+        // q.add(0);
+        // vis[0] = true;
+
+        // while (!q.isEmpty()) {
+        // int node = q.poll();
+        // bfs.add(node);
+
+        // // get all adjacent vertices
+        // // if adj is not visited then traversal and then mark has visited
+
+        // for (int it : adj.get(node)) {
+        // if (vis[node] == false) {
+        // vis[it] = true;
+        // q.add(it);
+        // }
+        // }
+        // }
+
+        // return bfs;
+
         ArrayList<Integer> bfs = new ArrayList<>();
         boolean[] vis = new boolean[V];
-
-
         Queue<Integer> q = new LinkedList<>();
-        // starting Node
+
         q.add(0);
         vis[0] = true;
 
@@ -49,13 +71,18 @@ public class Bfs {
             int node = q.poll();
             bfs.add(node);
 
-            // get all adjacent vertices
-            // if adj is not visited then traversal and then mark has visited
-        
-            for (int it : adj.get(node)){
-                if(vis[node] == false){
-                    vis[it] = true;
-                    q.add(it);
+            // for (Integer integer : adj.get(node)) {
+            // if (vis[integer] == false) {
+            // q.add(integer);
+            // vis[integer] = true;
+            // }
+            // }
+
+            for (int i = 0; i < adj.get(node).size(); i++) {
+                int temp = adj.get(node).get(i);
+                if (vis[temp] == false) {
+                    vis[temp] = true;
+                    q.add(temp);
                 }
             }
         }
