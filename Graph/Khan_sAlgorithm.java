5016cba84bdcdd5430ffedf880872157c562f3de
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-09-02 19:20:40 +0530

    Add Khan's Algorithm implementation for topological sorting in a directed graph

---
+++ Graph/Khan_sAlgorithm.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Khan_sAlgorithm {
+
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Adding edges
+        adj.get(5).add(0);
+        adj.get(5).add(2);
+        adj.get(4).add(0);
+        adj.get(4).add(1);
+        adj.get(2).add(3);
+        adj.get(3).add(1);
+
+        Khan_sAlgorithm obj = new Khan_sAlgorithm();
+
+        int[] ans = obj.topoSortKhanAlgo(V, adj);
+
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public int[] topoSortKhanAlgo(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        int[] inDegree = new int[V];
+
+        for (int i = 0; i < V; i++) {
+            for (int it : adj.get(i)) {
+                inDegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int[] topo = new int[V];
+        int idx = 0;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo[idx++] = node;
+
+            for (int it : adj.get(node)) {
+                inDegree[it]--;
+
+                if (inDegree[it] == 0) {
+                    q.add(it);
+                }
+            }
+        }
+
+        return topo;
+    }
+
+}
