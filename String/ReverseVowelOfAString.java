470e768b42fe9dd5242684d5f859608eacba3a5d
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-09-13 21:43:25 +0530

    Add ReverseVowelOfAString implementation to reverse vowels in a given string

---
+++ String/ReverseVowelOfAString.java
@@ -0,0 +1,43 @@
+package String;
+
+public class ReverseVowelOfAString {
+    public static void main(String[] args) {
+
+    }
+
+    public String reverseVowel(String s) {
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            while (left < right && !isVowel(arr[left])) {
+                left++;
+            }
+
+            while (left < right && !isVowel(arr[right])) {
+                right--;
+            }
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+
+    }
+
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' ||
+                c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' ||
+                c == 'O' || c == 'U';
+    }
+
+}
